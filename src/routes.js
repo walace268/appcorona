c20a4f4df156eb45782344f92cb931da72afca00
walace268 <walace.dilan@gmail.com>
2020-06-17 14:48:20 -0300

    finalização

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from  "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Appstack = createStackNavigator();
+
+import Home from './pages/Home';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator screenOpitions={{headerShown: false}}>
+                <Appstack.screen name="Home" component={Home}></Appstack.screen>
+            </AppStack.Navigator>       
+        </NavigationContainer> 
+    );
+}
